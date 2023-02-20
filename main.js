6ce548b82221f6443f65b0902b47d85b3504cd3e
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-02-20 22:52:05 +0200

    add events (start)

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ let currentDay = date.getDate();
 let list = document.querySelector(".days-list")
 let currentDate = document.querySelector(".current-data")
 let nextPrevBtn = document.getElementById("arrow-buttons");
+const days = document.querySelectorAll('.days-item');
+
+const activityInput = document.getElementById('activity');
+const form = modal.querySelector('form');
 
 function calendarRender() {
     let daysInMonth = new Date(currentYear, currentMonth+1, 0).getDate();
@@ -48,3 +52,81 @@ nextPrevBtn.addEventListener("click", icon => {
 });
 
 
+const events = {};
+
+function getEventsForDate(date) {
+return events[date] || [];
+}
+
+function updateModal(date) {
+const activityInput = modal.querySelector('#activity');
+const activities = getEventsForDate(date);
+let activitiesList = '';
+if (activities.length > 0) {
+activitiesList = activities.map(activity => <li>${activity}</li>).join('');
+}
+modal.querySelector('ul').innerHTML = activitiesList;
+modal.style.display = 'block';
+activityInput.focus();
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const activity = activityInput.value;
+    const activeDay = document.querySelector('.lol');
+    const date = activeDay.getAttribute('data-date');
+    
+    if (!events[date]) {
+    events[date] = [];
+    }
+    events[date].push(activity);
+    
+    activeDay.setAttribute('title', activity);
+    modal.style.display = 'none';
+    });
+    
+    // const days = document.querySelectorAll('.day');
+
+    days.forEach(day => {
+        day.addEventListener('click', event => {
+          // Remove active class from all days
+          days.forEach(day => {
+            day.classList.remove('lol');
+          });
+          
+          // Add active class to the clicked day
+          day.classList.add('lol');
+          
+          const date = day.getAttribute('data-date');
+          updateModal(date);
+        });
+      });
+
+      // Add event listener to form submit button
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        const activity = activityInput.value;
+        const activeDay = document.querySelector('.active');
+      
+        // Set the title attribute of the active day with the activity value
+        activeDay.setAttribute('title', activity);
+      
+        // Store the activity for the date
+        const activities = JSON.parse(localStorage.getItem(date)) || [];
+        activities.push(activity);
+        localStorage.setItem(date, JSON.stringify(activities));
+      
+        // Hide the modal
+        modal.style.display = 'none';
+      });
+
+      function updateModal(date) {
+        const activities = JSON.parse(localStorage.getItem(date)) || [];
+        const activityList = modal.querySelector('.activity-list');
+        activityList.innerHTML = '';
+        activities.forEach(act => {
+            const li = document.createElement('li');
+            li.textContent = act;
+            activityList.appendChild(li);
+            });
+        }
